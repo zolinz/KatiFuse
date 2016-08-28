7d60a9cb98c8e9a8603c1358eef6df520387d505
ZOLTANKOVACS <zoltankovacs@nbnco.com.au>
2016-08-28 20:19:01 +1000

    hello2

--- CamelRequestReplyServer/src/test/java/com/mycompany/camel/blueprint/RouteTest.java
+++ CamelRequestReplyServer/src/test/java/com/mycompany/camel/blueprint/RouteTest.java
@@ -1,22 +1,53 @@
 package com.mycompany.camel.blueprint;
 
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.model.RouteDefinition;
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RouteTest extends CamelBlueprintTestSupport {
-	
+    public static final Logger LOG = LoggerFactory.getLogger(RouteTest.class);
+
     @Override
     protected String getBlueprintDescriptor() {
         return "/OSGI-INF/blueprint/blueprint.xml";
     }
 
-    @Ignore
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        RouteDefinition routeDef = context.getRouteDefinition("provider-route-back");
+        try {
+            routeDef.adviceWith(context, new AdviceWithRouteBuilder() {
+                @Override
+                public void configure() {
+                    interceptSendToEndpoint("amq:queue:zoli.out").skipSendToOriginalEndpoint().to("mock:zoli.out");
+                }
+
+
+            });
+            routeDef.adviceWith(context, new AdviceWithRouteBuilder() {
+                @Override
+                public void configure() {
+                    replaceFromWith("mock:zoli.in");
+                }
+            });
+        } catch (Exception e) {
+            LOG.error("Error interceptSendTo", e);
+        }
+    }
+
     @Test
     public void testRoute() throws Exception {
+
+
+
+
         // the route is timer based, so every 5th second a message is send
         // we should then expect at least one message
-        getMockEndpoint("mock:result").expectedMinimumMessageCount(1);
+        getMockEndpoint("mock:zoli.out").expectedMinimumMessageCount(1);
 
         // assert expectations
         assertMockEndpointsSatisfied();
